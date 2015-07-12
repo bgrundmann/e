006efeb1fb3188b5637f64ac84e2ec43546cb5d1
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2015-07-12 18:35:22 +0100

    Record / Replay events and cpuprofiling

--- e.go
+++ e.go
@@ -5,6 +5,11 @@ import "github.com/bgrundmann/e/buf"
 import "github.com/bgrundmann/e/motion"
 import "io"
 import "os"
+import "flag"
+import "fmt"
+import "log"
+import "encoding/json"
+import "runtime/pprof"
 
 type View struct {
 	buffer        *buf.Buf // views may share same buffer
@@ -94,7 +99,7 @@ func (v *View) Display() {
 
 // AppendFile appends the contents of file to buf.
 func AppendFile(buf *buf.Buf, filename string) error {
-	f, err := os.Open("e.go")
+	f, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
@@ -103,23 +108,134 @@ func AppendFile(buf *buf.Buf, filename string) error {
 	return err
 }
 
-func main() {
+type RunMode int
+const (
+	RunModeRegular RunMode = iota
+	RunModeRecord
+	RunModeReplay
+) 
+
+type commandLineArgs struct {
+	runMode RunMode
+	recordingFile string // name of the file to record/replay
+	cpuprofile string
+	initialFiles []string
+} 
+
+func parseCommandLine() commandLineArgs {
+	var recordFile, replayFile string
+	var args commandLineArgs
+	flag.StringVar(&recordFile, "record", "", "record all events to file")
+	flag.StringVar(&replayFile, "replay", "", "replay all events from file")
+	flag.StringVar(&args.cpuprofile, "cpuprofile", "", "write cpu profile to file")
+	flag.Parse()
+	args.runMode = RunModeRegular
+	if recordFile != "" && replayFile != "" {
+		fmt.Fprintf(os.Stderr, "Must specify only one of record/replay!\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	} else if recordFile != "" {
+		args.runMode = RunModeRecord
+		args.recordingFile = recordFile
+	} else if replayFile != "" {
+		args.runMode = RunModeReplay
+		args.recordingFile = replayFile
+	} 
+	args.initialFiles = flag.Args()
+	return args
+} 
+
+// All init* functions below setup some part of the subsystem and return at least
+// a cleanup function that should be run when main exits (via defer).
+
+func initTermbox() func() {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
-	defer termbox.Close()
 	termbox.SetInputMode(termbox.InputEsc)
+	return termbox.Close
+} 
+
+func initEventSource(args commandLineArgs) (nextEvent func() termbox.Event, cleanup func()) {
+	switch args.runMode {
+	case RunModeRegular:
+		// nothing to be done
+		return termbox.PollEvent, func() {}
+	case RunModeReplay:
+		f, err := os.Open(args.recordingFile)
+		if err != nil {
+			log.Fatal(err)
+		} 
+		dec := json.NewDecoder(f)
+		return func() termbox.Event {
+			var ev *termbox.Event
+			if err := dec.Decode(&ev); err != nil {
+				log.Fatal(err)
+			}
+			return *ev
+		} , func() {
+			f.Close()
+		}
+	case RunModeRecord:
+		f, err := os.OpenFile(args.recordingFile, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+		enc := json.NewEncoder(f)
+		return func() termbox.Event {
+			ev := termbox.PollEvent()
+			if err := enc.Encode(&ev); err != nil {
+				log.Fatal(err)
+			} 
+			return ev
+		}, func() {
+			f.Close()
+		} 
+	default:
+		panic("Unknown run mode!")
+	} 
+} 
+
+func initBufferAndView(v *View, args commandLineArgs) func() {
 	var b buf.Buf
 	b.Init()
-	var v View
 	v.Init(&b)
-	AppendFile(&b, "e.go")
+	if len(args.initialFiles) > 0 {
+		if err := AppendFile(&b, args.initialFiles[0]); err != nil {
+			log.Fatal(err)
+		} 
+	} 
+	return func() {}
+} 
+
+func initProfiling(args commandLineArgs) func() {
+	if args.cpuprofile != "" {
+		f, err := os.Create(args.cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		} 
+		pprof.StartCPUProfile(f)
+		return pprof.StopCPUProfile
+	} else {
+		return func() {}
+	} 
+} 
+
+func main() {
+	args := parseCommandLine()
+	cleanup := initTermbox(); defer cleanup()
+	nextEvent, cleanup := initEventSource(args); defer cleanup()
+	var v View
+	cleanup = initBufferAndView(&v, args); defer cleanup()
+	// not that interested in startup and tear down cost
+	// so let's start profiling only now
+	cleanup = initProfiling(args); defer cleanup()
 
 mainloop:
 	for {
 		v.Display()
-		switch ev := termbox.PollEvent(); ev.Type {
+		switch ev := nextEvent(); ev.Type {
 		case termbox.EventKey:
 			switch ev.Key {
 			case termbox.KeyEsc:
